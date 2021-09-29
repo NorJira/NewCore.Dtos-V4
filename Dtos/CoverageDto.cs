7c8c3684b8d38e90f5a49bf2dba93db71a357d03
norjira <norjira@gmail.com>
2021-09-29 08:15:39 +0700

    Init V3

---
+++ Dtos/CoverageDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NewCore.Dtos
+{
+    public record CoverageDto
+    {
+        [Key]
+        public int cvgRef { get; set; }
+        [Required]
+        [StringLength(15)]
+        public string polId { get; set; }
+        [Required]
+        [StringLength(3)]
+        public string cvgNo { get; set; }
+        [Required]
+        [StringLength(5)]
+        public string cvgStatus { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime cvgCommDt { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime cvgExpryDate { get; set; }
+        [Required]
+        [StringLength(15)]
+        public string customerId { get; set; }
+        [Required]
+        [StringLength(15)]
+        public string planId { get; set; }
+        [Column(TypeName = "decimal(14, 2)")]
+        public decimal cvgFaceAmt { get; set; }
+        [Column(TypeName = "decimal(14, 2)")]
+        public decimal cvgPremAmt { get; set; }     
+    }
+}
---
+++ Dtos/PolCvgDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using NewCore.Dtos;
+
+namespace NewCore.Dtos
+{
+    public record PolCvgDto
+    {
+        public PolicyDto polInfo { get; set; }
+        public IEnumerable<CoverageDto> cvgsInfo { get; set; }
+    }
+}
